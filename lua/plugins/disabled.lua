181ad3ef3514a852bbe3c7c08d257de905676857
lucioreiz <lucio.000@hotmail.com>
2023-01-30 19:37:29 -0300

    ADD: Session Manager

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -7,4 +7,33 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
 
   { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "karb94/neoscroll.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "nacro90/numb.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+  {
+    "Shatur/neovim-session-manager",
+    ---@diagnostic disable-next-line: unused-local
+    opts = function(_, opts)
+      return {
+        -- sessions_dir = Path:new(vim.fn.stdpath('data'), 'sessions'), -- The directory where the session files will be saved.
+        -- path_replacer = '__', -- The character to which the path separator will be replaced for session files.
+        -- colon_replacer = '++', -- The character to which the colon symbol will be replaced for session files.
+        autoload_mode = require("session_manager.config").AutoloadMode.Disabled, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+        autosave_last_session = true, -- Automatically save last session on exit.
+        -- autosave_ignore_not_normal = true, -- Plugin will not save a session when no writable and listed buffers are opened.
+        -- autosave_only_in_session = false, -- Always autosaves session. If true, only autosaves after a session is active.
+      }
+    end,
+    cmd = "SessionManager",
+    event = "VeryLazy",
+    ft = "alpha",
+  },
 }
