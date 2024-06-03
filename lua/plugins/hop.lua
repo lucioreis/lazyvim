a7d95c0746ec941d2accd33412ef5e0a2def7e39
lucioreiz <lucio.000@hotmail.com>
2024-06-03 03:23:55 -0300

    Reconfig nvim hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,9 +1,80 @@
 local M = {
-  "phaazon/hop.nvim",
-  branch = "v2",
-  keys = { { "s","<CMD>HopWord<CR>", mode = {"n", "v"}, desc = "HopWord" } },
-  config = function()
-    require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+  "smoka7/hop.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "s",
+      function()
+        require("hop").hint_words()
+      end,
+      mode = { "n", "v" },
+      desc = "HopWord",
+    },
+    {
+      "x",
+      function()
+        require("hop").hint_char2()
+      end,
+      mode = { "n", "v" },
+      desc = "HopChar2",
+    },
+    {
+      "f",
+      function()
+        require("hop").hint_char1({
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Hop Same Line After Cursor",
+      silent = true,
+    },
+    {
+      "F",
+      function()
+        require("hop").hint_char1({
+          direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+          current_line_only = true,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Hop Same Line Before Cursor",
+      silent = true,
+    },
+    {
+      "t",
+      function()
+        require("hop").hint_char1({
+          direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Hop Same Line After Cursor",
+      silent = true,
+    },
+    {
+      "T",
+      function()
+        require("hop").hint_char1({
+          direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+          current_line_only = true,
+          hint_offset = -1,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Hop Same Line Before Cursor",
+      silent = true,
+    },
+  },
+
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
+  config = function(_, opts)
+    require("hop").setup(opts)
   end,
 }
 return M
